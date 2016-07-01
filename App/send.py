f7c97037da89913e2ba725607eba2eb8b8a0c086
LopsanAMO <lmolinao1200@alumno.ipn.mx>
2016-07-01 04:23:46 -0500

    weas

--- App/send.py
+++ App/send.py
@@ -1,11 +1,12 @@
-from  twilio.rest import TwilioRestClient
+from twilio.rest import TwilioRestClient
+from twilio import twiml
 
 def mensajes(mensaje):
     ACCOUNT_SID = "ACd29836c6dfff4b8edbf3114ebe698938"
     AUTH_TOKEN = "5a2088fc51efa56bd511240a1fba498b"
 
     client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-    
+
     client.messages.create(
     	to="+525569626596",
     	from_="12513334049",
