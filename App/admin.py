1303316808c73c1246a4edbf99a0e66c96821acf
Ubuntu <ubuntu@ip-172-31-20-148.us-west-2.compute.internal>
2016-07-01 14:04:23 +0000

    Again

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Trabajo 
 
 # Register your models here.
+@admin.register(Trabajo)
+
+class adminTrabajo(admin.ModelAdmin):
+    list_display = ('colonia',)
